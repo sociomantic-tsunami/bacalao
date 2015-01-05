9e77f580decf9af0a17d8b3e1f0fdf79cc499435
Daniel Norman <daniel.norman@sociomantic.com>
2015-01-05 12:05:09 +0100

    Compose a frozen config object from the config params in the config.json. More convinient than a JSON file when multiple config keys repeat.

---
+++ server/config/config.js
@@ -0,0 +1,31 @@
+var config = require('../../config.json');
+
+config.sessionAuth = {
+  password: config.cookieSecret,
+  cookie: config.cookieKey,
+  redirectTo: '/',
+  isSecure: false
+};
+
+config.facebookAuth = {
+  provider: 'facebook',
+  password: config.cookieSecret,
+  clientId: config.facebookAppId,
+  clientSecret: config.facebookAppSecret,
+  isSecure: false
+};
+
+config.statehood = {
+  encoding: 'iron',
+  password: config.cookieSecret,
+  isSecure: false
+};
+
+
+Object.freeze(config);
+Object.freeze(config.sessionAuth);
+Object.freeze(config.facebookAuth);
+Object.freeze(config.statehood);
+
+
+module.exports = config;
