1977901be259985ba6dce14c725ebab41c21aa56
davide-ramaglietta-sociomantic <davide.ramaglietta@sociomantic.com>
2015-05-08 13:02:25 +0200

    create event with minimum 1 max attendee. For #17

--- src/js/components/NewEventForm.react.jsx
+++ src/js/components/NewEventForm.react.jsx
@@ -58,6 +58,7 @@ var NewEventForm = React.createClass({
             <Input
               type="number"
               label="Max Attendees"
+              min="1"
               value={this.state.maxAttendees}
               onChange={this._onMaxAttendeesChange} />
             <Input
@@ -138,7 +139,10 @@ var NewEventForm = React.createClass({
   },
 
   _onMaxAttendeesChange: function(e) {
-    if ( e.target.value % 1 === 0 ) {
+    if ( e.target.value == 0 ) {
+      this.setState({ maxAttendees: parseInt(1, 10) });
+    }
+    else if ( e.target.value % 1 === 0 ) {
       this.setState({ maxAttendees: parseInt(e.target.value, 10) });
     } else {
       this.setState({ maxAttendees: Math.floor(e.target.value)});
