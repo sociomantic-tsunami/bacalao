54f33a83943df45f2e948541e8e5b3de014277c8
davide-ramaglietta-sociomantic <davide.ramaglietta@sociomantic.com>
2015-05-08 18:38:36 +0200

    warn the user about that he is joining two events at the same time. For #17

--- src/js/components/UpcomingEvents.react.jsx
+++ src/js/components/UpcomingEvents.react.jsx
@@ -43,15 +43,41 @@ var UpcomingEvents = React.createClass({
     };
   },
 
+  hasMultipleEvents: function( upcomingEvents ) {
+    var multipleEvents = true;
+    if( upcomingEvents.length > 0 ) {
+      for(var i=0; i < upcomingEvents.length-1; i++) {
+        for(var j=i+1; j < upcomingEvents.length; j++) {
+          if( upcomingEvents[i] && upcomingEvents[j]
+              && upcomingEvents[i]._store.props.upcomingEvent.time.toString()
+              == upcomingEvents[j]._store.props.upcomingEvent.time.toString() )
+            multipleEvents = multipleEvents && false;
+        }
+      }
+    }
+    return !multipleEvents;
+  },
+
   render: function () {
     var upcomingEvents = _.map(this.state.upcomingEvents, getUpcomingEvent);
-
-    return (
-        <div className="upcoming-events">
-          <h4>Upcoming</h4>
-            {upcomingEvents}
-        </div>
-    );
+    var multipleEvents = this.hasMultipleEvents( upcomingEvents );
+    if( multipleEvents ) {
+      return (
+          <div className="upcoming-events">
+            <div className="warning">You are attending more than one event at the same time!</div>
+            <h4>Upcoming</h4>
+              {upcomingEvents}
+          </div>
+      );
+    }
+    else {
+      return (
+          <div className="upcoming-events">
+            <h4>Upcoming</h4>
+              {upcomingEvents}
+          </div>
+      );
+    }
   }
 });
 
